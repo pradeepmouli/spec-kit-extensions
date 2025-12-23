7c8e6a52b94089435cd9348466fdf80e16094957
Pradeep Mouli <pmouli@mac.com>
2025-12-23 17:35:33 -0500

    Bump version to 1.4.4

--- specify_extend.py
+++ specify_extend.py
@@ -43,7 +43,7 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.progress import Progress, SpinnerColumn, TextColumn
 
-__version__ = "1.4.3"
+__version__ = "1.4.4"
 
 # Initialize Rich console
 console = Console()
